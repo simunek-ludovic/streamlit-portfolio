199d5e0d3b49d20254af0b91d294d79ae15c1efb
ludovic-simunek <ludovic@simunek.fr>
2025-01-14 09:39:35 +0100

    Mise à jour de l'application Streamlit avec des correctifs

--- page_github.py
+++ page_github.py
@@ -2,6 +2,13 @@
 st.header("Git / Github")
 
 st.write("Git est un logiciel open-source qui permet de gérer les différentes versions d’un projet (code, documents, etc...).")
+st.write("""
+- Suivi des modifications : Git garde un historique complet de toutes les modifications apportées aux fichiers d’un projet. Chaque modification est enregistrée comme une "version" ou "commit".
+- Travail collaboratif : Plusieurs développeurs peuvent travailler sur un même projet sans écraser le travail des autres.
+- Branchements (branches) : Permet de créer des "branches" pour travailler sur différentes fonctionnalités ou corrections de bugs en parallèle, sans affecter le projet principal.
+""")
+
+
 st.write("GitHub est une plateforme en ligne qui permet de stocker et de partager des projets Git.")
 
 # Code

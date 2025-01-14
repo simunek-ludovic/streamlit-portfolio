8cd0d7c170e7d2b7ad0f79ebd9f146d4373a05b2
ludovic-simunek <ludovic@simunek.fr>
2025-01-14 09:53:26 +0100

    Mise à jour de l'application Streamlit avec des correctifs

--- page_github.py
+++ page_github.py
@@ -8,23 +8,95 @@ st.write("""
 - Branchements (branches) : Permet de créer des "branches" pour travailler sur différentes fonctionnalités ou corrections de bugs en parallèle, sans affecter le projet principal.
 """)
 
-
-
-
-
 # Code
 with st.expander("Initialiser le dépot Git"):
     st.code("""
     git init
     """, language="bash")
 
+# Code
+with st.expander("Ajoute un fichier spécifique au suivi de version"):
+    st.code("""
+    git add nom_fichier
+    """, language="bash")
 
+# Code
+with st.expander("Ajoute tous les fichiers au suivi de version"):
+    st.code("""
+    git add .
+    """, language="bash")
 
+# Code
+with st.expander("Enregistrer une modification (commit)"):
+    st.code("""
+    git commit -m "Message du commit"
+    """, language="bash")
 
+# Code
+with st.expander("Vérifier l'état du dépôt"):
+    st.code("""
+    git status
+    """, language="bash")
 
+# Code
+with st.expander("Afficher l’historique des commits"):
+    st.code("""
+    git log
+    """, language="bash")
 
+# Code
+with st.expander("Relier un dépôt local à GitHub"):
+    st.code("""
+    git remote add origin https://github.com/username/nom_du_depot.git
+    """, language="bash")
 
+# Code
+with st.expander("Envoyer des modifications vers GitHub"):
+    st.code("""
+    git push -u origin main
+    """, language="bash")
 
+# Code
+with st.expander("Récupérer les modifications depuis GitHub"):
+    st.code("""
+    git pull origin main
+    """, language="bash")
+
+# Code
+with st.expander("Crée une nouvelle branche pour travailler sur une nouvelle fonctionnalité ou un correctif"):
+    st.code("""
+    git branch nom_branche
+    """, language="bash")
+
+# Code
+with st.expander("Changer de branche"):
+    st.code("""
+    git checkout nom_branche
+    """, language="bash")
+
+# Code
+with st.expander("Fusionner une branche dans la branche courante"):
+    st.code("""
+    git merge nom_branche
+    """, language="bash")
+
+# Code
+with st.expander("Cloner un dépôt existant"):
+    st.code("""
+    git clone https://github.com/username/nom_du_depot.git
+    """, language="bash")
+
+# Code
+with st.expander("Supprime un fichier du suivi de version Git"):
+    st.code("""
+    git rm nom_fichier
+    """, language="bash")
+
+# Code
+with st.expander("Annuler des modifications"):
+    st.code("""
+    git checkout -- nom_fichier
+    """, language="bash")
 
 # Créer un trait de séparation
 st.markdown("""
@@ -38,13 +110,9 @@ st.markdown("""
     <hr>
     """, unsafe_allow_html=True)
 
-st.write("GitHub est une plateforme en ligne qui permet de stocker et de partager des projets Git. Les fonctionnalités principales de GitHub sont :")
+st.write("Git est un logiciel open-source qui permet de gérer les différentes versions d’un projet (code, documents, etc...). Les fonctionnalités principales de Git sont :")
 st.write("""
-- Hébergement des dépôts : GitHub stocke vos projets Git dans le cloud, accessibles depuis n’importe où.
-Travail collaboratif : Les équipes peuvent travailler ensemble sur des projets en utilisant des outils comme les "pull requests" et les "issues".
-Partage public ou privé : Les projets peuvent être publics (visibles par tout le monde) ou privés (accès restreint).
+- Suivi des modifications : Git garde un historique complet de toutes les modifications apportées aux fichiers d’un projet. Chaque modification est enregistrée comme une "version" ou "commit".
+- Travail collaboratif : Plusieurs développeurs peuvent travailler sur un même projet sans écraser le travail des autres.
+- Branchements (branches) : Permet de créer des "branches" pour travailler sur différentes fonctionnalités ou corrections de bugs en parallèle, sans affecter le projet principal.
 """)
-
-
-
-

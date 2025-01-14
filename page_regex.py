e0d889a70e4ee00f12865d0f8e79a0a9df33e563
ludovic-simunek <ludovic@simunek.fr>
2025-01-14 10:20:16 +0100

    Mise à jour de l'application Streamlit avec des correctifs

--- page_regex.py
+++ page_regex.py
@@ -96,11 +96,11 @@ st.markdown("""
 st.write("### Exemple de recherches")
 with st.expander("Code Regex"):
     st.code("""
-    user_input = st.text_input("Entrez une chaîne (peu importe chiffres, lettres (minuscules et/ou majuscules), caractères spéciaux, ...) pour rechercher les chiffres :")
+    user_input = st.text_input("Entrez une chaîne (...) pour rechercher les chiffres :")
     chiffres = re.findall(r"\d+", user_input)
     """, language='python')
 
-user_input = st.text_input("Entrez une chaîne pour rechercher les chiffres :")
+user_input = st.text_input("Entrez une chaîne (peu importe chiffres, lettres (minuscules et/ou majuscules), caractères spéciaux, ...) pour rechercher les chiffres :")
 
 if user_input:
     # Exemple de recherche de chiffres

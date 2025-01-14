1aab92a60df7061d1b835525ae9b4aae67f8b6aa
ludovic-simunek <ludovic@simunek.fr>
2025-01-14 09:37:31 +0100

    Mise à jour de l'application Streamlit avec des correctifs

--- page_github.py
+++ page_github.py
@@ -4,11 +4,10 @@ st.header("Git / Github")
 st.write("Git est un logiciel open-source qui permet de gérer les différentes versions d’un projet (code, documents, etc...).")
 st.write("GitHub est une plateforme en ligne qui permet de stocker et de partager des projets Git.")
 
+# Code
+with st.expander("Initialiser le dépot Git"):
+    st.code("""
+    git init
+    """, language="bash")
 
-st.code("""
-# Naviguer vers votre projet
-cd chemin/vers/votre/projet
 
-# Initialiser le dépôt Git
-git init
-""", language="bash")

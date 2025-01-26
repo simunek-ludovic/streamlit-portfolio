eba209910df1c86aa791c2ec27e681d537c1fe71
ludovic-simunek <ludovic@simunek.fr>
2025-01-26 20:52:02 +0100

    Ajout CV (jpg + pdf)

---
+++ page_cv.py
@@ -0,0 +1,6 @@
+# PAGE github
+# Titre de la page
+st.header("Curriculum Vitae")
+
+
+st.image("https://raw.githubusercontent.com/simunek-ludovic/streamlit-portfolio/refs/heads/main/images/Toys%26models_00.png", use_container_width=True)

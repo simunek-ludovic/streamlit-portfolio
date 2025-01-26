683e1735ffb07d09e3618191a3df1fe4c73030c2
ludovic-simunek <ludovic@simunek.fr>
2025-01-26 21:15:11 +0100

    MAJ 'Contactez moi'

--- page_cv.py
+++
@@ -1,6 +0,0 @@
-# PAGE github
-# Titre de la page
-st.header("Curriculum Vitae")
-
-
-st.image("https://raw.githubusercontent.com/simunek-ludovic/streamlit-portfolio/refs/heads/main/images/Toys%26models_00.png", use_container_width=True)

43eeaa7b706547f4bb344738bde9689569ddae6c
ludovic-simunek <ludovic@simunek.fr>
2025-03-30 09:21:36 +0200

    Ajout page du projet Boussole Lilloise

--- page_projets.py
+++ page_projets.py
@@ -2,7 +2,7 @@
 import streamlit as st
 
 # Barre de navigation
-page_selection = st.sidebar.selectbox("Choisissez un des projets :", ["Toys & Models", "Recommandations de films"], index=0)
+page_selection = st.sidebar.selectbox("Choisissez un des projets :", ["Toys & Models", "Recommandations de films","La Boussole Lilloise"], index=0)
 
 if page_selection == "Recommandations de films":
     st.header("Recommandations de films")
@@ -15,5 +15,13 @@ elif page_selection == "Toys & Models":
     st.header("Projet Toys & Models")
     # Exécuter le code du fichier python en utilisant exec()
     with open("projet_toys&models.py", "r", encoding="utf-8") as f:
+        content = f.read()
+    exec(content)
+
+
+elif page_selection == "La Boussole Lilloise":
+    st.header("La Boussole Lilloise")
+    # Exécuter le code du fichier python en utilisant exec()
+    with open("projet_boussole_lilloise.py", "r", encoding="utf-8") as f:
         content = f.read()
     exec(content)
---
+++ projet_boussole_lilloise.py
@@ -0,0 +1,13 @@
+# Contenu selon le choix du sous-menu
+
+st.write("### Présentation")
+
+st.write("La Boussole Lilloise est une application permettant de consulter en temps réel et en prévisionnel les informations sur la météo ainsi que les différents modes de transport disponibles dans la Métropole Lilloise.")
+
+st.write("### Ressources :")
+st.write("Plusieurs API ont été intégrées pour la réalisation de cette application.")
+st.write("Pour la météo, vous avez accès à des prévisions et à des relevés en temps réel, actualisés toutes les 15 minutes, pour les 24 heures à venir.")
+st.write("Quant aux modes de transport disponibles dans la Métropole Lilloise, vous pouvez consulter les informations concernant V'Lille, les bus, le métro, le tramway, les informations de trafic, les parkings relais et les aires de covoiturage.")
+st.write("Grâce à cette application, vous pouvez choisir le mode de transport le plus adapté en fonction de la météo.")
+
+st.markdown("Pour découvrir l'application, visitez : [https://lsimunek.pythonanywhere.com/](https://lsimunek.pythonanywhere.com/).")
